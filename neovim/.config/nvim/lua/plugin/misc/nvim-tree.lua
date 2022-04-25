736a3b6686e094c23608225f7610c4063961fe72
Feiyou Guo <feiyouguo@gmail.com>
2022-04-24 23:13:39 -0700

    refactor: update nvim-tree config

--- neovim/.config/nvim/lua/plugin/misc/nvim-tree.lua
+++ neovim/.config/nvim/lua/plugin/misc/nvim-tree.lua
@@ -83,25 +83,55 @@ return {
     -- each of these are documented in `:help nvim-tree.OPTION_NAME`
     nvim_tree.setup {
       disable_netrw       = true,
-      hijack_netrw        = true,
       open_on_setup       = true,
-      ignore_ft_on_setup  = {},
-      -- auto_close          = true,
-      open_on_tab         = true,
+      hide_root_folder    = false,
       hijack_cursor       = false,
+      hijack_netrw        = true,
+      open_on_tab         = true,
       update_cwd          = true,
-      update_to_buf_dir   = {
+      view                = {
+        width = 30,
+        height = 30,
+        side = 'left',
+        preserve_window_proportions = true,
+        number = false,
+        relativenumber = false,
+        signcolumn = "yes",
+        mappings = {
+          custom_only = true,
+          list = custom_mapping
+        },
+      },
+      renderer            = {
+        indent_markers = {
+          enable = true,
+          icons = {
+            corner = "└ ",
+            edge = "│ ",
+            none = "  ",
+          },
+        },
+        icons = {
+          webdev_colors = true,
+        },
+      },
+      hijack_directories  = {
         enable = true,
         auto_open = true,
       },
+      update_focused_file = {
+        enable      = true,
+        update_cwd  = true,
+        ignore_list = {}
+      },
+      ignore_ft_on_setup  = {},
+      system_open         = {
+        cmd  = nil,
+        args = {}
+      },
       diagnostics         = {
         enable = true,
-        -- icons = {
-        -- hint = "",
-        -- info = "",
-        -- warning = "",
-        -- error = "",
-        --}
+        show_on_dirs = true,
         icons = {
           hint = "",
           info = "",
@@ -110,55 +140,45 @@ return {
         }
 
       },
-      update_focused_file = {
-        enable      = true,
-        update_cwd  = true,
-        ignore_list = {}
-      },
-      system_open         = {
-        cmd  = nil,
-        args = {}
-      },
       filters             = {
         dotfiles = false,
-        custom = {}
+        custom = {},
+        exclude = {},
       },
       git                 = {
         enable = true,
         ignore = true,
-        timeout = 500,
+        timeout = 400,
       },
-      view                = {
-        width = 30,
-        height = 30,
-        hide_root_folder = false,
-        side = 'left',
-        auto_resize = true,
-        mappings = {
-          custom_only = true,
-          list = custom_mapping
+      actions             = {
+        use_system_clipboard = true,
+        change_dir = {
+          enable = true,
+          global = false,
+          restrict_above_cwd = false,
         },
-        number = false,
-        relativenumber = false,
-        signcolumn = "yes"
+        open_file = {
+          quit_on_open = false,
+          resize_window = false,
+          window_picker = {
+            enable = true,
+            chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+            exclude = {
+              filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+              buftype = { "nofile", "terminal", "help" },
+            },
+          }
+        }
       },
       trash               = {
         cmd = "trash",
         require_confirm = true
       },
-      actions             = {
-        change_dir = {
-          global = false,
-        },
-        open_file = {
-          quit_on_open = false,
-        }
-      }
     }
   end,
 
   defer = function()
-      -- MARK: Register and add to command_center
+    -- MARK: Register and add to command_center
     local has_command_center, command_center = pcall(require, "command_center")
     if not has_command_center then return end
 

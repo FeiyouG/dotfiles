66060495f1e64120de750aa1d117cfc404a1e262
Feiyou Guo <feiyouguo@gmail.com>
2022-08-10 11:01:16 -0400

    update(nvim/markdown): configure vim-table-mode plugin

--- neovim/.config/nvim/lua/plugin/markdown/init.lua
+++ neovim/.config/nvim/lua/plugin/markdown/init.lua
@@ -1,7 +1,7 @@
 return {
   require("plugin/markdown/markdown-preview"),
   require("plugin/markdown/vim-markdown-toc"),
-  { 'dhruvasagar/vim-table-mode' },
+  require("plugin/markdown/vim-table-mode"),
 
   require("plugin/markdown/zk-nvim"), -- Integration with zk tool
 }
---
+++ neovim/.config/nvim/lua/plugin/markdown/vim-table-mode.lua
@@ -0,0 +1,7 @@
+return {
+  'dhruvasagar/vim-table-mode',
+
+  config = function()
+    vim.cmd "let g:table_mode_corner='|'"
+  end
+}

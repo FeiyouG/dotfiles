eee9b8e3047f6115e3206662423c3517c027e4ce
Feiyou Guo <feiyouguo@gmail.com>
2022-10-16 09:25:00 -0700

    feat(zsh): install homebrew automatically with zinit

--- zsh/.config/zsh/src/programs/homebrew.zsh
+++ zsh/.config/zsh/src/programs/homebrew.zsh
@@ -1,1 +1,11 @@
-
+zi light-mode wait lucid for \
+    as'null' \
+    atclone'%atpull' \
+    atpull'
+         ./bin/brew update --preinstall \
+      && ln -sf $PWD/completions/zsh/_brew $ZINIT[COMPLETIONS_DIR]' \
+    depth'3' \
+    nocompletions \
+    sbin'bin/brew' \
+    eval"./bin/brew shellenv" \
+  homebrew/brew

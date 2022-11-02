bc13d3ecaccc57d4b76f9e6defc15446dcd517bd
Feiyou Guo <feiyouguo@gmail.com>
2022-11-01 21:07:18 -0700

    update(nvim): use more vanilla vim keymaps

--- neovim/.config/nvim/lua/core/mappings.lua
+++ neovim/.config/nvim/lua/core/mappings.lua
@@ -21,9 +21,9 @@ map("v", "<M-Up>", ":m '<-2<CR>gv=gv", noremap)
 map("v", "<M-Down>", ":m '>+1<CR>gv=gv", noremap)
 
 -- Press i to enter insert mode, and ii to exit insert mode.
-map("i", "ii", "<Esc>", noremap)
-map("i", "jk", "<Esc>", noremap)
-map("i", "kj", "<Esc>", noremap)
+-- map("i", "ii", "<Esc>", noremap)
+-- map("i", "jk", "<Esc>", noremap)
+-- map("i", "kj", "<Esc>", noremap)
 
 -- filepath and pwd to clipboar by yf and yd respectivley
 map("n", "yf", "<cmd>let @+=expand('%:p')<CR>", silent_noremap)
@@ -31,40 +31,42 @@ map("n", "yd", "<cmd>let @+=expand('%:p:h')<CR>", silent_noremap)
 
 -- MISC
 map("n", "<leader>r", "<cmd>source $MYVIMRC<CR>", noremap) -- Shortcur for refresh nvim without restarting
-map("n", "<leader>w", "<cmd>w<CR>", noremap)               -- Shortcut for save
-map("n", "<leader>p", "\"_DP", noremap)                    -- Paste without yank delted text
+map("n", "<leader>w", "<cmd>w<CR>", noremap) -- Shortcut for save
+map("n", "<leader>p", "\"_DP", noremap) -- Paste without yank delted text
 
+map("n", "}", '<CMD>execute "keepjumps norm! " . v:count1 . "}"<CR>', noremap)
+map("n", "{", '<CMD>execute "keepjumps norm! " . v:count1 . "{"<CR>', noremap)
 
 
 ---- MARK: NAVIGATING ----
 -- Move faster through the document
-map("n", "<leader>j", "10j", noremap)
-map("n", "<leader>k", "10k", noremap)
-map("v", "<leader>j", "10j", noremap)
-map("v", "<leader>k", "10k", noremap)
-
--- Move faster through the line
-map("n", "<leader>h", "10h", noremap)
-map("n", "<leader>l", "10l", noremap)
-map("v", "<leader>h", "10h", noremap)
-map("v", "<leader>l", "10l", noremap)
+-- map("n", "<leader>j", "10j", noremap)
+-- map("n", "<leader>k", "10k", noremap)
+-- map("v", "<leader>j", "10j", noremap)
+-- map("v", "<leader>k", "10k", noremap)
+--
+-- -- Move faster through the line
+-- map("n", "<leader>h", "10h", noremap)
+-- map("n", "<leader>l", "10l", noremap)
+-- map("v", "<leader>h", "10h", noremap)
+-- map("v", "<leader>l", "10l", noremap)
 
 -- Move up/down without moving on-screen cursor position
-map("n", "<C-M-y>", "<C-y>k", noremap)
-map("v", "<C-M-y>", "<C-y>k", noremap)
-map("n", "<C-M-e>", "j<C-e>", noremap)
-map("n", "<C-M-e>", "j<C-e>", noremap)
+-- map("n", "<C-M-y>", "<C-y>k", noremap)
+-- map("v", "<C-M-y>", "<C-y>k", noremap)
+-- map("n", "<C-M-e>", "j<C-e>", noremap)
+-- map("n", "<C-M-e>", "j<C-e>", noremap)
 
 
 -- Move to the beginning and end of line
-map("n", "H", "^", noremap)
-map("n", "L", "$", noremap)
-map("v", "H", "^", noremap)
-map("v", "L", "$", noremap)
+-- map("n", "H", "^", noremap)
+-- map("n", "L", "$", noremap)
+-- map("v", "H", "^", noremap)
+-- map("v", "L", "$", noremap)
 
 -- Delete from the curor to the beginning/end of the line
-map("n", "dH", "d^", noremap)
-map("n", "dL", "d$", noremap)
+-- map("n", "dH", "d^", noremap)
+-- map("n", "dL", "d$", noremap)
 
 
 ---- MARK: Buffer Management ----

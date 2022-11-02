9858bd24499d27aae40c814c65a3c77968d1170c
Feiyou Guo <feiyouguo@gmail.com>
2022-11-01 21:10:55 -0700

    update(nvim): remove lightspeed; use smart-split

--- neovim/.config/nvim/lua/plugin/navigation/init.lua
+++ neovim/.config/nvim/lua/plugin/navigation/init.lua
@@ -1,9 +1,10 @@
 return {
   require("plugin.navigation.nvim-tree"),
   -- require("plugin.navigation.neo-tree"),
-  require("plugin.navigation.lightspeed"),
+  -- require("plugin.navigation.lightspeed"),
   require("plugin.navigation.trouble"),
   require("plugin.navigation.windows"),
   require("plugin.navigation.navic"),
+  require("plugin.navigation.smart-split"),
   { "wellle/targets.vim" }
 }

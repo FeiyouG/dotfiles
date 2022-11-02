09c7ef012b166029a399ebbaa505ac747e0d0438
Feiyou Guo <feiyouguo@gmail.com>
2022-11-01 21:09:15 -0700

    feat(nvim): add prettier and stylua with null-ls

--- neovim/.config/nvim/lua/plugin/lsp/null-ls.lua
+++ neovim/.config/nvim/lua/plugin/lsp/null-ls.lua
@@ -7,9 +7,14 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
+        -- MARK: Formatter
         null_ls.builtins.formatting.isort,
         null_ls.builtins.formatting.black,
-
+        null_ls.builtins.formatting.prettier.with({
+          filetype = { "javascript", "javascriptreact", "typescript", "typescriptreact", "css", "html", "json", "jsonc",
+            "yaml" }
+        }),
+        null_ls.builtins.formatting.stylua,
       }
     })
   end

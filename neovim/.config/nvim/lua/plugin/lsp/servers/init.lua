000c117669c536a6fcd11da9401872b11d633638
Feiyou Guo <feiyouguo@gmail.com>
2023-01-16 00:13:47 -0800

    feat(nvim): add texlab lsp

--- neovim/.config/nvim/lua/plugin/lsp/servers/init.lua
+++ neovim/.config/nvim/lua/plugin/lsp/servers/init.lua
@@ -1,12 +1,13 @@
 return {
   nvim_lspconfig = {
-    ltex = require("plugin.lsp.servers.ltex"),
     sumneko_lua = require("plugin.lsp.servers.sumneko_lua"),
     gopls = require("plugin.lsp.servers.gopls"),
     beancount = require("plugin.lsp.servers.beancount"),
     lemminx = require("plugin.lsp.servers.lemminx"),
     pylsp = require("plugin.lsp.servers.pylsp"),
+    ltex = require("plugin.lsp.servers.ltex"),
 
+    texlab = {},
     ocamllsp = {},
     tsserver = {},
     vimls = {},

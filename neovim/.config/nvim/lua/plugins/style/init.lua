e6645fa31421f4494f57885564789aa32d48bb17
Feiyou Guo <feiyouguo@gmail.com>
2023-01-25 22:11:12 -0800

    feat(nvim): add zen-mode

--- neovim/.config/nvim/lua/plugins/style/init.lua
+++ neovim/.config/nvim/lua/plugins/style/init.lua
@@ -1,12 +1,13 @@
 return {
-  require("plugins.style.onenord"),
-  require("plugins.style.lualine"),
-  require("plugins.style.nvim-ufo"),
-  require("plugins.style.dressing"),
-  require("plugins.style.neoscroll"),
-  require("plugins.style.colorizer"),
-  require("plugins.style.todo-comments"),
-  { "fladson/vim-kitty", ft = { "kitty" } }, -- Syntax highlight for kitty config files
-  { "lukas-reineke/indent-blankline.nvim" },
-  { "godlygeek/tabular" },
+	require("plugins.style.onenord"),
+	require("plugins.style.lualine"),
+	require("plugins.style.nvim-ufo"),
+	require("plugins.style.dressing"),
+	require("plugins.style.neoscroll"),
+	require("plugins.style.colorizer"),
+	require("plugins.style.todo-comments"),
+	require("plugins.style.zen-mode"),
+	{ "fladson/vim-kitty", ft = { "conf" } }, -- Syntax highlight for kitty config files
+	{ "lukas-reineke/indent-blankline.nvim" },
+	{ "godlygeek/tabular" },
 }

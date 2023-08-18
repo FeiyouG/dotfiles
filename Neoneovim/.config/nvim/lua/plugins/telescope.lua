57c8dd92cd07c2d2c8c9d16031affe49982d5c17
Feiyou Guo <feiyouguo@gmail.com>
2023-08-17 23:59:59 -0700

    feat: add trouble.nvim configuration

--- Neoneovim/.config/nvim/lua/plugins/telescope.lua
+++ Neoneovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,199 +1,258 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = {
-			"folke/trouble.nvim",
-			"nvim-lua/plenary.nvim",
-			{
-				"nvim-telescope/telescope-dap.nvim",
-				config = function()
-					require("telescope").load_extension("dap")
-				end,
-			},
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "make",
-				config = function()
-					require("telescope").load_extension("fzf")
-				end,
-			},
-		},
-		config = function()
-			local telescope = require("telescope")
-			local actions = require("telescope.actions")
-			local trouble = require("trouble.providers.telescope")
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "folke/trouble.nvim",
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-dap.nvim",
+        config = function()
+          require("telescope").load_extension("dap")
+        end,
+      },
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        config = function()
+          require("telescope").load_extension("fzf")
+        end,
+      },
+    },
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
 
-			telescope.setup({
-				defaults = {
-					layout_strategy = "flex",
-					mappings = {
-						n = {
-							["q"] = actions.close,
-							["<Esc>"] = actions.close,
-							["<C-n>"] = actions.move_selection_next,
-							["<C-p>"] = actions.move_selection_previous,
-							["<C-t>"] = actions.select_tab,
-							["<C-f>"] = actions.preview_scrolling_up,
-							["<C-b>"] = actions.preview_scrolling_down,
-							["<C-q>"] = trouble.open_with_trouble,
-						},
-						i = {
-							["<Esc>"] = actions.close,
-							["<C-u>"] = false, -- Clear the prompt
-							["<C-s>"] = actions.select_horizontal,
-							["<C-t>"] = actions.select_tab,
-							["<C-f>"] = actions.preview_scrolling_up,
-							["<C-b>"] = actions.preview_scrolling_down,
-							["<C-q>"] = trouble.open_with_trouble,
-						},
-					},
-					file_ignore_patterns = {
-						"node_modules",
-					},
-				},
-				pickers = {
-					lsp_code_actions = {
-						theme = "cursor",
-					},
-				},
-			})
+      -- Setup telescope config
+      local telescope_config = {
+        defaults = {
+          layout_strategy = "flex",
+          mappings = {
+            n = {
+              ["q"] = actions.close,
+              ["<Esc>"] = actions.close,
+              ["<C-n>"] = actions.move_selection_next,
+              ["<C-p>"] = actions.move_selection_previous,
+              ["<C-t>"] = actions.select_tab,
+              ["<C-f>"] = actions.preview_scrolling_up,
+              ["<C-b>"] = actions.preview_scrolling_down,
+            },
+            i = {
+              ["<Esc>"] = actions.close,
+              ["<C-u>"] = false, -- Clear the prompt
+              ["<C-s>"] = actions.select_horizontal,
+              ["<C-t>"] = actions.select_tab,
+              ["<C-f>"] = actions.preview_scrolling_up,
+              ["<C-b>"] = actions.preview_scrolling_down,
+            },
+          },
+          file_ignore_patterns = {
+            "node_modules",
+          },
+        },
+        pickers = {
+          lsp_code_actions = {
+            theme = "cursor",
+          },
+        },
+      }
 
-			vim.keymap.set("n", "<leader>ff", "<CMD>Telescope find_files<CR>", { desc = "Find files" })
-			vim.keymap.set("n", "<leader>fk", "<CMD>Telescope keymaps<CR>", { desc = "Find keymaps" })
-			vim.keymap.set("n", "<leader>fb", "<CMD>Telescope buffers<CR>", { desc = "Find buffers" })
-			vim.keymap.set("n", "<leader>fg", "<CMD>Telescope live_grep<CR>", { desc = "Find string in workspace" })
-			vim.keymap.set("n", "<leader>fl", "<CMD>Telescope current_buffer_fuzzy_find<CR>", { desc = "Find line in current buffer" })
-			-- settings.fn.keymap.set({
-			--   {
-			--     desc = "Find hidden files",
-			--     cmd = "<CMD>Telescope find_files hidden=true<CR>",
-			--   },
-			--   {
-			--     desc = "Show recent files",
-			--     cmd = "<CMD>Telescope oldfiles<CR>",
-			--   },
-			--   {
-			--     desc = "Show all commands",
-			--     cmd = "<CMD>Telescope commands<CR>",
-			--   },
-			--   {
-			--     desc = "Show command history",
-			--     cmd = "<CMD>Telescope command_history<CR>",
-			--   },
-			--   {
-			--     desc = "Show search history (vimgrep)",
-			--     cmd = "<CMD>Telescope search_history<CR>",
-			--   },
-			--   {
-			--     desc = "Show marks",
-			--     cmd = "<CMD>Telescope marks<CR>",
-			--   },
-			--   {
-			--     desc = "Switch colorschemes",
-			--     cmd = "<CMD>Telescope colorscheme<CR>",
-			--   },
-			--   {
-			--     desc = "Show jumplist",
-			--     cmd = "<CMD>Telescope jumplist<CR>",
-			--   },
-			--   {
-			--     desc = "Edit vim options",
-			--     cmd = "<CMD>Telescope vim_options<CR>",
-			--   },
-			--   {
-			--     desc = "Show autocommands",
-			--     cmd = "<CMD>Telescope autocommands<CR>",
-			--   },
-			--   {
-			--     desc = "Show telescope builtin commands",
-			--     cmd = "<CMD>Telescope builtin<CR>",
-			--   },
-			--
-			--   -- Git Pickers
-			--   {
-			--     desc = "Show workspace git commits",
-			--     cmd = "<CMD>Telescope git_commits<CR>",
-			--   },
-			--   {
-			--     desc = "Show git branches",
-			--     cmd = "<CMD>Telescope <CR>",
-			--   },
-			--   {
-			--     desc = "Show git stash",
-			--     cmd = "<CMD>Telescope git_stash<CR>",
-			--   },
-			-- })
+      if (settings.fn.is_installed("trouble.nvim")) then
+        local trouble = require("trouble.providers.telescope")
+        telescope_config.defaults.mappings.n["<C-q>"] = trouble.open_with_trouble
+        telescope_config.defaults.mappings.i["<C-q>"] = trouble.open_with_trouble
+      end
 
-			vim.list_extend(settings.ft.exclude_winbar.filetype, {
-				"TelescopePrompt",
-				"TelescopeResults",
-			})
-		end,
-	},
-	{
-		"axieax/urlview.nvim",
-		dependencies = {
-			"nvim-telescope/telescope.nvim",
-		},
-		config = function()
-			require("urlview").setup({
-				default_picker = "telescope",
-				default_prefix = "https://",
-				default_action = "system",
-				unique = true,
-				sorted = true,
-				jump = {
-					["prev"] = "[u",
-					["next"] = "]u",
-				},
-			})
+      telescope.setup(telescope_config)
 
-			vim.keymap.set({ "n", "v" }, "<leader>fu", "<Cmd>UrlView<CR>", { desc = "Show all buffer links" })
-			vim.keymap.set({ "n", "v" }, "<leader>fub", "<Cmd>UrlView<CR>", { desc = "Show all buffer links" })
-			vim.keymap.set({ "n", "v" }, "<leader>fup", "<Cmd>UrlView lazy<CR>", { desc = "Show all plugin links" })
-		end,
-	},
+      -- Setup keymaps
+      vim.keymap.set("n", "<leader>ff", "<CMD>Telescope find_files<CR>", { desc = "Find files" })
+      vim.keymap.set("n", "<leader>fk", "<CMD>Telescope keymaps<CR>", { desc = "Find keymaps" })
+      vim.keymap.set("n", "<leader>fb", "<CMD>Telescope buffers<CR>", { desc = "Find buffers" })
+      vim.keymap.set("n", "<leader>fg", "<CMD>Telescope live_grep<CR>", { desc = "Find string in workspace" })
+      vim.keymap.set("n", "<leader>fl", "<CMD>Telescope current_buffer_fuzzy_find<CR>",
+        { desc = "Find line in current buffer" })
+      -- settings.fn.keymap.set({
+      --   {
+      --     desc = "Find hidden files",
+      --     cmd = "<CMD>Telescope find_files hidden=true<CR>",
+      --   },
+      --   {
+      --     desc = "Show recent files",
+      --     cmd = "<CMD>Telescope oldfiles<CR>",
+      --   },
+      --   {
+      --     desc = "Show all commands",
+      --     cmd = "<CMD>Telescope commands<CR>",
+      --   },
+      --   {
+      --     desc = "Show command history",
+      --     cmd = "<CMD>Telescope command_history<CR>",
+      --   },
+      --   {
+      --     desc = "Show search history (vimgrep)",
+      --     cmd = "<CMD>Telescope search_history<CR>",
+      --   },
+      --   {
+      --     desc = "Show marks",
+      --     cmd = "<CMD>Telescope marks<CR>",
+      --   },
+      --   {
+      --     desc = "Switch colorschemes",
+      --     cmd = "<CMD>Telescope colorscheme<CR>",
+      --   },
+      --   {
+      --     desc = "Show jumplist",
+      --     cmd = "<CMD>Telescope jumplist<CR>",
+      --   },
+      --   {
+      --     desc = "Edit vim options",
+      --     cmd = "<CMD>Telescope vim_options<CR>",
+      --   },
+      --   {
+      --     desc = "Show autocommands",
+      --     cmd = "<CMD>Telescope autocommands<CR>",
+      --   },
+      --   {
+      --     desc = "Show telescope builtin commands",
+      --     cmd = "<CMD>Telescope builtin<CR>",
+      --   },
+      --
+      --   -- Git Pickers
+      --   {
+      --     desc = "Show workspace git commits",
+      --     cmd = "<CMD>Telescope git_commits<CR>",
+      --   },
+      --   {
+      --     desc = "Show git branches",
+      --     cmd = "<CMD>Telescope <CR>",
+      --   },
+      --   {
+      --     desc = "Show git stash",
+      --     cmd = "<CMD>Telescope git_stash<CR>",
+      --   },
+      -- })
 
-	-- {
-	--   "git@github.com:FeiyouG/command_center.nvim.git",
-	--   dev = true,
-	--   priority = settings.priority.HIGH,
-	--   dependencies = {
-	--     "nvim-telescope/telescope.nvim",
-	--   },
-	--   config = function()
-	--     local commander = require("commander")
-	--     commander.setup({
-	--       components = {
-	--         commander.component.DESC,
-	--         commander.component.KEYS,
-	--         commander.component.CAT,
-	--       },
-	--       sort_by = {
-	--         commander.component.DESC,
-	--         commander.component.CAT,
-	--         commander.component.KEYS,
-	--         commander.component.CMD,
-	--       },
-	--       auto_replace_desc_with_cmd = true,
-	--       telescope = {
-	--         integrate = true,
-	--         theme = require("telescope.themes").commander,
-	--       },
-	--     })
-	--
-	--     settings.fn.keymap.set({
-	--       {
-	--         desc = "Open Commander",
-	--         cmd = "<CMD>Telescope commander<CR>",
-	--         keys = {
-	--           -- If ever hesitate when using telescope start with <leader>f,
-	--           -- also open command center
-	--           { { "n", "x" }, "<Leader>f" },
-	--           { { "n", "x" }, "<Leader>fc" },
-	--         },
-	--       },
-	--     })
-	--   end,
-	-- },
+      -- Filetype update
+      vim.list_extend(settings.ft.exclude_winbar.filetype, {
+        "TelescopePrompt",
+        "TelescopeResults",
+      })
+    end,
+  },
+  {
+    "axieax/urlview.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("urlview").setup({
+        default_picker = "telescope",
+        default_prefix = "https://",
+        default_action = "system",
+        unique = true,
+        sorted = true,
+        jump = {
+          ["prev"] = "[u",
+          ["next"] = "]u",
+        },
+      })
+
+      vim.keymap.set({ "n", "v" }, "<leader>fu", "<Cmd>UrlView<CR>", { desc = "Show all buffer links" })
+      vim.keymap.set({ "n", "v" }, "<leader>fub", "<Cmd>UrlView<CR>", { desc = "Show all buffer links" })
+      vim.keymap.set({ "n", "v" }, "<leader>fup", "<Cmd>UrlView lazy<CR>", { desc = "Show all plugin links" })
+    end,
+  },
+  {
+    "folke/trouble.nvim",
+
+    event = { "VeryLazy" },
+
+    keys = {
+      { "<leader>xx", "<CMD>TroubleToggle<CR>",                       desc = "Toggle trouble" },
+      { "<leader>xq", "<CMD>TroubleToggle quickfix<CR>",              desc = "Toggle quickfix with trouble" },
+      { "<leader>xl", "<CMD>TroubleToggle loclist<CR>",               desc = "Toggle quickfix with loclist" },
+      { "<leader>xw", "<CMD>TroubleToggle workspace_diagnostics<CR>", desc = "Toggle workspace diagnostic with loclist" },
+      { "<leader>xd", "<CMD>TroubleToggle document_diagnostics<CR>",  desc = "Toggle document diagnostic with loclist" },
+      {
+        "]x",
+        function() require("trouble").next({ skip_groups = true, jump = true }) end,
+        desc = "Go to next item in trouble"
+      },
+      {
+        "[x",
+        function() require("trouble").previous({ skip_groups = true, jump = true }) end,
+        desc = "Go to previous item in trouble"
+      },
+    },
+
+    opts = {
+      mode = "quickfix",
+      fold_open = settings.icons.fs.folder.indicator.open,
+      fold_closed = settings.icons.fs.folder.indicator.closed,
+      padding = false,
+      action_keys = {
+        refresh = "<c-r>",
+        open_split = "<c-s>",
+        jump_close = "gf",
+        switch_severity = "<c-f>",
+        toggle_preview = "P",
+        hover = "K",
+        preview = "p",
+        open_code_href = "c",
+        help = "?"
+      },
+      win_config = { border = settings.icons.editor.border.rounded },
+      auto_jump = {},
+      signs = {
+        error = settings.icons.diagnostic.error,
+        warning = settings.icons.diagnostic.warning,
+        hint = settings.icons.diagnostic.hint,
+        information = settings.icons.diagnostic.info,
+        other = settings.icons.diagnostic.other
+      },
+    }
+  }
+
+  -- {
+  --   "git@github.com:FeiyouG/command_center.nvim.git",
+  --   dev = true,
+  --   priority = settings.priority.HIGH,
+  --   dependencies = {
+  --     "nvim-telescope/telescope.nvim",
+  --   },
+  --   config = function()
+  --     local commander = require("commander")
+  --     commander.setup({
+  --       components = {
+  --         commander.component.DESC,
+  --         commander.component.KEYS,
+  --         commander.component.CAT,
+  --       },
+  --       sort_by = {
+  --         commander.component.DESC,
+  --         commander.component.CAT,
+  --         commander.component.KEYS,
+  --         commander.component.CMD,
+  --       },
+  --       auto_replace_desc_with_cmd = true,
+  --       telescope = {
+  --         integrate = true,
+  --         theme = require("telescope.themes").commander,
+  --       },
+  --     })
+  --
+  --     settings.fn.keymap.set({
+  --       {
+  --         desc = "Open Commander",
+  --         cmd = "<CMD>Telescope commander<CR>",
+  --         keys = {
+  --           -- If ever hesitate when using telescope start with <leader>f,
+  --           -- also open command center
+  --           { { "n", "x" }, "<Leader>f" },
+  --           { { "n", "x" }, "<Leader>fc" },
+  --         },
+  --       },
+  --     })
+  --   end,
+  -- },
 }
--- Neoneovim/.config/nvim/lua/settings/fn.lua
+++ Neoneovim/.config/nvim/lua/settings/fn.lua
@@ -4,23 +4,37 @@ _G.settings.fn = M
 ---@param obj any the object to be checked
 ---@return boolean is_callable true if obj is callable
 function M.is_callable(obj)
-	if type(obj) == "function" then
-		return true
-	elseif type(obj) == "table" then
-		local mt = getmetatable(obj)
-		if mt then
-			return type(mt.__call) == "function"
-		end
-	end
-	return false
+  if type(obj) == "function" then
+    return true
+  elseif type(obj) == "table" then
+    local mt = getmetatable(obj)
+    if mt then
+      return type(mt.__call) == "function"
+    end
+  end
+  return false
 end
 
----Return the name of the root directory of project based on patterns
+---Return the name of the root directory of the current project based on patterns
 ---@param patterns {[integer]: string} | nil files/dirs that the root dir would contain; look for ,git folder by default
 ---@return string path the path of the root directory
 function M.project_root(patterns)
-	patterns = patterns or { ".git" }
-	return vim.fs.dirname(vim.fs.find(patterns, { upward = true })[1])
+  patterns = patterns or { ".git" }
+  return vim.fs.dirname(vim.fs.find(patterns, { upward = true })[1])
+end
+
+local plugins = nil
+
+---Return true if plugin is installed (may not be loaded)
+---@param plugin_name string the name of the plugin to be checked
+function M.is_installed(plugin_name)
+  if not plugins then
+    plugins = {}
+    for _, plugin_config in ipairs(require("lazy").plugins()) do
+      plugins[plugin_config.name] = plugin_config
+    end
+  end
+  return plugins[plugin_name] ~= nil
 end
 
 local os_name = nil
@@ -28,24 +42,24 @@ local os_name = nil
 ---Get current OS name
 ---@return string os_name one of "mac", "win", "linux", and "unkown"
 function M.os_name()
-	if os_name ~= nil then
-		return os_name
-	end
+  if os_name ~= nil then
+    return os_name
+  end
 
-	local os_list = {
-		mac = "mac",
-		win32 = "win",
-		linux = "linux",
-	}
+  local os_list = {
+    mac = "mac",
+    win32 = "win",
+    linux = "linux",
+  }
 
-	for os, name in pairs(os_list) do
-		if vim.fn.has(os) == 1 then
-			os_name = name
-			return name
-		end
-	end
+  for os, name in pairs(os_list) do
+    if vim.fn.has(os) == 1 then
+      os_name = name
+      return name
+    end
+  end
 
-	return "unkown"
+  return "unkown"
 end
 
 -- function M.safe_access(t, ...)
@@ -94,99 +108,99 @@ local capabilities = nil
 ---Get the lsp client capabilities
 ---@return table capabilities
 M.lsp.get_capabilities = function()
-	if capabilities then
-		return capabilities
-	end
+  if capabilities then
+    return capabilities
+  end
 
-	capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities = vim.lsp.protocol.make_client_capabilities()
 
-	local cmp_avail, cmp = pcall(require, "cmp_nvim_lsp")
-	if cmp_avail then
-		capabilities.textDocument.completion = cmp.default_capabilities().textDocument.completion
-	end
+  local cmp_avail, cmp = pcall(require, "cmp_nvim_lsp")
+  if cmp_avail then
+    capabilities.textDocument.completion = cmp.default_capabilities().textDocument.completion
+  end
 
   local snip_avail, _ = pcall(require, "luasnip")
   if snip_avail then
     capabilities.textDocument.completion.completionItem.snippetSupport = true
   end
 
-	local ufo_avil, _ = pcall(require, "ufo")
-	if ufo_avil then
-		capabilities.textDocument.foldingRange = {
-			dynamicRegistration = false,
-			lineFoldingOnly = true,
-		}
-	end
+  local ufo_avil, _ = pcall(require, "ufo")
+  if ufo_avil then
+    capabilities.textDocument.foldingRange = {
+      dynamicRegistration = false,
+      lineFoldingOnly = true,
+    }
+  end
 
-	return capabilities
+  return capabilities
 end
 
 ---on_attach method shared by all language servers
 M.lsp.on_attach = function(client, bufnr)
-	if client.server_capabilities.documentFormattingProvider then
-		vim.keymap.set("n", "<leader>sf", function()
-			-- Trim trailing white space first
-			if not vim.o.binary and vim.o.filetype ~= "diff" and vim.bo.modifiable then
-				vim.cmd([[%s/\s\+$//e]])
-				vim.cmd("noh")
-			end
-			vim.lsp.buf.format({ async = true })
-		end, { desc = "Format code (lint)" })
-		-- 	M.keymap.set({
-		-- 		{
-		-- 			description = "Format code (lint)",
-		-- 			cmd = function()
-		-- 				-- Trim trailing white space first
-		-- 				if not vim.o.binary and vim.o.filetype ~= "diff" and vim.bo.modifiable then
-		-- 					vim.cmd([[%s/\s\+$//e]])
-		-- 					vim.cmd("noh")
-		-- 				end
-		--
-		-- 				vim.lsp.buf.format({ async = true })
-		-- 			end,
-		-- 			keybindings = { "n", "<leader>sf" },
-		-- 		},
-		-- 	})
-	end
+  if client.server_capabilities.documentFormattingProvider then
+    vim.keymap.set("n", "<leader>sf", function()
+      -- Trim trailing white space first
+      if not vim.o.binary and vim.o.filetype ~= "diff" and vim.bo.modifiable then
+        vim.cmd([[%s/\s\+$//e]])
+        vim.cmd("noh")
+      end
+      vim.lsp.buf.format({ async = true })
+    end, { desc = "Format code (lint)" })
+    -- 	M.keymap.set({
+    -- 		{
+    -- 			description = "Format code (lint)",
+    -- 			cmd = function()
+    -- 				-- Trim trailing white space first
+    -- 				if not vim.o.binary and vim.o.filetype ~= "diff" and vim.bo.modifiable then
+    -- 					vim.cmd([[%s/\s\+$//e]])
+    -- 					vim.cmd("noh")
+    -- 				end
+    --
+    -- 				vim.lsp.buf.format({ async = true })
+    -- 			end,
+    -- 			keybindings = { "n", "<leader>sf" },
+    -- 		},
+    -- 	})
+  end
 
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show documentations (hover)" })
-	vim.keymap.set("n", "E", vim.diagnostic.open_float, { desc = "Show errors of the current line (floating window)" })
-	vim.keymap.set("n", "<leader>sk", vim.lsp.buf.signature_help, { desc = "Show function signature" })
-	vim.keymap.set("n", "<leader>sD", vim.lsp.buf.declaration, { desc = "Go to declarations" })
-	vim.keymap.set("n", "<leader>sn", vim.lsp.buf.rename, { desc = "Rename symbol" })
-	vim.keymap.set("n", "<leader>sd", "<CMD>Telescope lsp_definitions<CR>", { desc = "Go to definitions" })
-	vim.keymap.set("n", "<leader>st", "<CMD>Telescope lsp_type_definitions<CR>", { desc = "Go to type definitions" })
-	vim.keymap.set("n", "<leader>sr", "<CMD>Telescope lsp_references<CR>", { desc = "Show all references" })
-	vim.keymap.set(
-		"n",
-		"<leader>ssw",
-		"<CMD>Telescope lsp_dynamic_workspace_symbols<CR>",
-		{ desc = "show workspace symbols" }
-	)
-	vim.keymap.set("n", "<leader>si", "<CMD>Telescope lsp_implementations<CR>", { desc = "Go to implementations" })
-	vim.keymap.set("n", "<leader>sen", vim.diagnostic.goto_next, { desc = "Go to the next diagnostic item" })
-	vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to the next diagnostic item" })
-	vim.keymap.set("n", "<leader>sep", vim.diagnostic.goto_prev, { desc = "Go to the previous diagnostic item" })
-	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to the previous diagnostic item" })
-	vim.keymap.set({ "n", "v" }, "<leader>sa", vim.lsp.buf.code_action, { desc = "Show code actions" })
-	vim.keymap.set("n", "<leader>se", "<CMD>Telescope diagnostics<CR>", { desc = "Show workspace errors (diagnostic)" })
-	vim.keymap.set(
-		"n",
-		"<leader>sef",
-		"<CMD>Telescope diagnostics<CR>",
-		{ desc = "Show workspace errors (diagnostic)" }
-	)
-	vim.keymap.set("n", "<leader>ssd", "<CMD>Telescope lsp_document_symbols<CR>", { desc = "Show document symbols" })
-	vim.keymap.set("n", "<leader>ss", "<CMD>Telescope lsp_document_symbols<CR>", { desc = "Show document symbols" })
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show documentations (hover)" })
+  vim.keymap.set("n", "E", vim.diagnostic.open_float, { desc = "Show errors of the current line (floating window)" })
+  vim.keymap.set("n", "<leader>sk", vim.lsp.buf.signature_help, { desc = "Show function signature" })
+  vim.keymap.set("n", "<leader>sD", vim.lsp.buf.declaration, { desc = "Go to declarations" })
+  vim.keymap.set("n", "<leader>sn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+  vim.keymap.set("n", "<leader>sd", "<CMD>Telescope lsp_definitions<CR>", { desc = "Go to definitions" })
+  vim.keymap.set("n", "<leader>st", "<CMD>Telescope lsp_type_definitions<CR>", { desc = "Go to type definitions" })
+  vim.keymap.set("n", "<leader>sr", "<CMD>Telescope lsp_references<CR>", { desc = "Show all references" })
+  vim.keymap.set(
+    "n",
+    "<leader>ssw",
+    "<CMD>Telescope lsp_dynamic_workspace_symbols<CR>",
+    { desc = "show workspace symbols" }
+  )
+  vim.keymap.set("n", "<leader>si", "<CMD>Telescope lsp_implementations<CR>", { desc = "Go to implementations" })
+  vim.keymap.set("n", "<leader>sen", vim.diagnostic.goto_next, { desc = "Go to the next diagnostic item" })
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to the next diagnostic item" })
+  vim.keymap.set("n", "<leader>sep", vim.diagnostic.goto_prev, { desc = "Go to the previous diagnostic item" })
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to the previous diagnostic item" })
+  vim.keymap.set({ "n", "v" }, "<leader>sa", vim.lsp.buf.code_action, { desc = "Show code actions" })
+  vim.keymap.set("n", "<leader>se", "<CMD>Telescope diagnostics<CR>", { desc = "Show workspace errors (diagnostic)" })
+  vim.keymap.set(
+    "n",
+    "<leader>sef",
+    "<CMD>Telescope diagnostics<CR>",
+    { desc = "Show workspace errors (diagnostic)" }
+  )
+  vim.keymap.set("n", "<leader>ssd", "<CMD>Telescope lsp_document_symbols<CR>", { desc = "Show document symbols" })
+  vim.keymap.set("n", "<leader>ss", "<CMD>Telescope lsp_document_symbols<CR>", { desc = "Show document symbols" })
 end
 
 M.keymap = {
-	-- set = function(keymaps, opts)
-	-- 	local commander_avail, commander = pcall(require, "commander")
-	-- 	if commander_avail then
-	-- 		commander.add(keymaps, opts)
-	-- 	end
-	-- end,
+  -- set = function(keymaps, opts)
+  -- 	local commander_avail, commander = pcall(require, "commander")
+  -- 	if commander_avail then
+  -- 		commander.add(keymaps, opts)
+  -- 	end
+  -- end,
 }
 
 return M

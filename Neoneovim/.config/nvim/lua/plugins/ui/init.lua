c2085ac940b6f8dfa7b1da9e4c55f1eeb44e7c78
Feiyou Guo <feiyouguo@gmail.com>
2023-08-27 23:54:22 -0700

    fix: dressing.nvim configuration error

--- Neoneovim/.config/nvim/lua/plugins/ui/init.lua
+++ Neoneovim/.config/nvim/lua/plugins/ui/init.lua
@@ -1,75 +1,73 @@
 return {
-	require("plugins.ui.statusline"),
-	require("plugins.ui.statuscolum"),
-	require("plugins.ui.winbar"),
-	{
-		"rebelot/heirline.nvim",
-		priority = settings.priority.MEDIUM,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-			"rmehri01/onenord.nvim",
-			"lewis6991/gitsigns.nvim",
-		},
-		config = true,
-	},
-	{
-		"stevearc/dressing.nvim",
-		event = { "VeryLazy" },
-		config = function()
-			local telescope_avil, telescope_theme = pcall(require, "telescope.theme")
+  require("plugins.ui.statusline"),
+  require("plugins.ui.statuscolum"),
+  require("plugins.ui.winbar"),
+  {
+    "rebelot/heirline.nvim",
+    priority = settings.priority.MEDIUM,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "rmehri01/onenord.nvim",
+      "lewis6991/gitsigns.nvim",
+    },
+    config = true,
+  },
+  {
+    "stevearc/dressing.nvim",
+    event = { "VeryLazy" },
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("dressing").setup({
+        input = {
+          mappings = {
+            n = {
+              ["<Esc>"] = "Close",
+              ["q"] = "Close",
+              ["<C-c>"] = "Close",
+              ["<CR>"] = "Confirm",
+            },
+            i = {
+              ["<C-c>"] = "Close",
+              ["<CR>"] = "Confirm",
+              ["<Up>"] = "HistoryPrev",
+              ["<Down>"] = "HistoryNext",
+            },
+          },
+          win_options = {
+            winblend = 5,
+          },
+        },
+        select = {
+          backend = { "telescope", "builtin" },
+          telescope = require("telescope.themes").get_cursor(),
+          win_options = {
+            winblend = 5,
+          },
+          mappings = {
+            ["<Esc>"] = "Close",
+            ["q"] = "Close",
+            ["<C-c>"] = "Close",
+            ["<CR>"] = "Confirm",
+          },
+        },
+      })
 
-			require("dressing").setup({
-				input = {
-					border = settings.icons.editor.border.rounded_with_hl,
-					mappings = {
-						n = {
-							["<Esc>"] = "Close",
-							["q"] = "Close",
-							["<C-c>"] = "Close",
-							["<CR>"] = "Confirm",
-						},
-						i = {
-							["<C-c>"] = "Close",
-							["<CR>"] = "Confirm",
-							["<Up>"] = "HistoryPrev",
-							["<Down>"] = "HistoryNext",
-						},
-						win_options = {
-							border = settings.icons.editor.border.rounded_with_hl,
-							winblend = 5,
-						},
-					},
-				},
-				select = {
-					backend = { "telescope", "builtin" },
-					telescope = telescope_avil and telescope_theme.get_cursor() or nil,
-					win_options = {
-						border = settings.icons.editor.border.rounded_with_hl,
-						winblend = 5,
-					},
-					mappings = {
-						["<Esc>"] = "Close",
-						["q"] = "Close",
-						["<C-c>"] = "Close",
-						["<CR>"] = "Confirm",
-					},
-				},
-			})
-
-			vim.list_extend(settings.ft.exclude_winbar.filetype, { "dressinginput", "dressingselect" })
-			vim.list_extend(settings.ft.quit_on_q.filetype, { "dressinginput", "dressingselect" })
-		end,
-	},
-	{
-		"karb94/neoscroll.nvim",
-		event = { "VeryLazy" },
-		config = function()
-			require("neoscroll").setup()
-			require("neoscroll.config").set_mappings({
-				["<C-y>"] = { "scroll", { "-1", "true", "100" } },
-				["<C-e>"] = { "scroll", { "1", "true", "100" } },
-			})
-		end,
-	},
-	{ "lukas-reineke/indent-blankline.nvim" },
+      vim.list_extend(settings.ft.exclude_winbar.filetype, { "dressinginput", "dressingselect" })
+      vim.list_extend(settings.ft.quit_on_q.filetype, { "dressinginput", "dressingselect" })
+    end,
+  },
+  {
+    "karb94/neoscroll.nvim",
+    event = { "VeryLazy" },
+    config = function()
+      require("neoscroll").setup()
+      require("neoscroll.config").set_mappings({
+        ["<C-y>"] = { "scroll", { "-1", "true", "100" } },
+        ["<C-e>"] = { "scroll", { "1", "true", "100" } },
+      })
+    end,
+  },
+  { "lukas-reineke/indent-blankline.nvim" },
 }

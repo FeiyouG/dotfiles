56868f7573e420ff0eedb0f9c20faf1d54c35abe
Feiyou Guo <feiyouguo@gmail.com>
2023-09-10 00:43:51 -0700

    update: add go.nvim

--- Neoneovim/.config/nvim/lua/plugins/lang/go.lua
+++ Neoneovim/.config/nvim/lua/plugins/lang/go.lua
@@ -1,37 +1,52 @@
 return {
-	{
-		"jose-elias-alvarez/null-ls.nvim",
-		opts = {
-			require("null-ls").builtins.formatting.gofumpt,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		opts = function(_, opts)
-			opts.gopls = {
-				cmd = { "gopls", "serve" },
-				filetypes = { "go", "gomod" },
-				root_dir = require("lspconfig.util").root_pattern("go.work", "go.mod", ".git"),
-				settings = {
-					gopls = {
-						analyses = {
-							unusedparams = true,
-						},
-						staticcheck = true,
-					},
-				},
-			}
-			return opts
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = function(_, opts)
-			opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, {
-				"go",
-				"gomod",
-			})
-			return opts
-		end,
-	},
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = {
+      require("null-ls").builtins.formatting.gofumpt,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts.gopls = {
+        cmd = { "gopls", "serve" },
+        filetypes = { "go", "gomod" },
+        root_dir = require("lspconfig.util").root_pattern("go.work", "go.mod", ".git"),
+        settings = {
+          gopls = {
+            analyses = {
+              unusedparams = true,
+            },
+            staticcheck = true,
+          },
+        },
+      }
+      return opts
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, {
+        "go",
+        "gomod",
+      })
+      return opts
+    end,
+  },
+  {
+    "ray-x/go.nvim",
+    event = { "CmdlineEnter" },
+    ft = { "go", 'gomod' },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+    dependencies = {                                        -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+  }
+
 }

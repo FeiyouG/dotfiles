71aec5bc79380457914189ef13454015c0e6d6eb
Feiyou Guo <feiyouguo@gmail.com>
2024-06-23 18:00:02 -0700

    Add <leader>fG to search workspace including dotfiles

--- neovim/.config/nvim/lua/plugins/navigation.lua
+++ neovim/.config/nvim/lua/plugins/navigation.lua
@@ -1,214 +1,226 @@
 return {
-  {
-    "folke/trouble.nvim",
-    event = { "VeryLazy" },
-    keys = {
-      { "<leader>xx", "<CMD>TroubleToggle<CR>",                       desc = "Toggle trouble" },
-      { "<leader>xq", "<CMD>TroubleToggle quickfix<CR>",              desc = "Toggle quickfix with trouble" },
-      { "<leader>xl", "<CMD>TroubleToggle loclist<CR>",               desc = "Toggle quickfix with loclist" },
-      { "<leader>xw", "<CMD>TroubleToggle workspace_diagnostics<CR>", desc = "Toggle workspace diagnostic with loclist" },
-      { "<leader>xd", "<CMD>TroubleToggle document_diagnostics<CR>",  desc = "Toggle document diagnostic with loclist" },
-      {
-        "]x",
-        function() require("trouble").next({ skip_groups = true, jump = true }) end,
-        desc = "Go to next item in trouble"
-      },
-      {
-        "[x",
-        function() require("trouble").previous({ skip_groups = true, jump = true }) end,
-        desc = "Go to previous item in trouble"
-      },
-    },
-    dependencies = {
-      {
-        "nvim-telescope/telescope.nvim",
-        opts = function(_, opts)
-          local trouble_provider = require("trouble.providers.telescope")
-          return vim.tbl_deep_extend("force", opts, {
-            defaults = {
-              mappings = {
-                n = { ["<C-q>"] = require("trouble.sources.telescope").open },
-                i = { ["<C-q>"] = require("trouble.sources.telescope").open }
-              }
-            }
-          })
-        end,
-      }
-    },
-    opts = {
-      mode = "quickfix",
-      fold_open = settings.icons.fs.folder.indicator.open,
-      fold_closed = settings.icons.fs.folder.indicator.closed,
-      padding = false,
-      action_keys = {
-        refresh = "<c-r>",
-        open_split = "<c-s>",
-        jump_close = "gf",
-        switch_severity = "<c-f>",
-        toggle_preview = "P",
-        hover = "K",
-        preview = "p",
-        open_code_href = "c",
-        help = "?"
-      },
-      win_config = { border = settings.icons.editor.border.rounded },
-      auto_jump = {},
-      signs = {
-        error = settings.icons.diagnostic.error,
-        warning = settings.icons.diagnostic.warning,
-        hint = settings.icons.diagnostic.hint,
-        information = settings.icons.diagnostic.info,
-        other = settings.icons.diagnostic.other
-      },
-    }
-  },
-  {
-    "anuvyklack/windows.nvim",
+	{
+		"folke/trouble.nvim",
+		event = { "VeryLazy" },
+		keys = {
+			{ "<leader>xx", "<CMD>TroubleToggle<CR>", desc = "Toggle trouble" },
+			{ "<leader>xq", "<CMD>TroubleToggle quickfix<CR>", desc = "Toggle quickfix with trouble" },
+			{ "<leader>xl", "<CMD>TroubleToggle loclist<CR>", desc = "Toggle quickfix with loclist" },
+			{
+				"<leader>xw",
+				"<CMD>TroubleToggle workspace_diagnostics<CR>",
+				desc = "Toggle workspace diagnostic with loclist",
+			},
+			{
+				"<leader>xd",
+				"<CMD>TroubleToggle document_diagnostics<CR>",
+				desc = "Toggle document diagnostic with loclist",
+			},
+			{
+				"]x",
+				function()
+					require("trouble").next({ skip_groups = true, jump = true })
+				end,
+				desc = "Go to next item in trouble",
+			},
+			{
+				"[x",
+				function()
+					require("trouble").previous({ skip_groups = true, jump = true })
+				end,
+				desc = "Go to previous item in trouble",
+			},
+		},
+		dependencies = {
+			{
+				"nvim-telescope/telescope.nvim",
+				opts = function(_, opts)
+					local trouble_provider = require("trouble.providers.telescope")
+					return vim.tbl_deep_extend("force", opts, {
+						defaults = {
+							mappings = {
+								n = { ["<C-q>"] = require("trouble.sources.telescope").open },
+								i = { ["<C-q>"] = require("trouble.sources.telescope").open },
+							},
+						},
+					})
+				end,
+			},
+		},
+		opts = {
+			mode = "quickfix",
+			fold_open = settings.icons.fs.folder.indicator.open,
+			fold_closed = settings.icons.fs.folder.indicator.closed,
+			padding = false,
+			action_keys = {
+				refresh = "<c-r>",
+				open_split = "<c-s>",
+				jump_close = "gf",
+				switch_severity = "<c-f>",
+				toggle_preview = "P",
+				hover = "K",
+				preview = "p",
+				open_code_href = "c",
+				help = "?",
+			},
+			win_config = { border = settings.icons.editor.border.rounded },
+			auto_jump = {},
+			signs = {
+				error = settings.icons.diagnostic.error,
+				warning = settings.icons.diagnostic.warning,
+				hint = settings.icons.diagnostic.hint,
+				information = settings.icons.diagnostic.info,
+				other = settings.icons.diagnostic.other,
+			},
+		},
+	},
+	{
+		"anuvyklack/windows.nvim",
 
-    event = { "VeryLazy" },
+		event = { "VeryLazy" },
 
-    dependencies = {
-      "anuvyklack/middleclass",
-      "anuvyklack/animation.nvim",
-    },
+		dependencies = {
+			"anuvyklack/middleclass",
+			"anuvyklack/animation.nvim",
+		},
 
-    keys = {
-      {
-        "<c-w>z",
-        "<CMD>WindowsMaximize<CR>",
-        desc = "Maximize current buffer with animation",
-        mode = "n",
-      },
-      {
-        "<C-w>|",
-        "<CMD>WindowsMaximizeHorizontally<CR>",
-        desc = "Maximize current buffer horizontally with animation",
-        mode = "n"
-      },
-      {
-        "<C-w>_",
-        "<CMD>WindowsMaximizeVertically<CR>",
-        desc = "Maximize current buffer Vertically with animation",
-        mode = "n",
-      },
-      {
-        "<C-w>=",
-        "<CMD>WindowsEqualize<CR>",
-        desc = "Equalize all buffers with animation",
-        mode = "n",
-      },
-      {
-        "<C-w>!",
-        "<CMD>WindowsToggleAutowidth<CR>",
-        desc = "Toggle Sliding buffer width",
-        mode = "n",
-      },
-    },
+		keys = {
+			{
+				"<c-w>z",
+				"<CMD>WindowsMaximize<CR>",
+				desc = "Maximize current buffer with animation",
+				mode = "n",
+			},
+			{
+				"<C-w>|",
+				"<CMD>WindowsMaximizeHorizontally<CR>",
+				desc = "Maximize current buffer horizontally with animation",
+				mode = "n",
+			},
+			{
+				"<C-w>_",
+				"<CMD>WindowsMaximizeVertically<CR>",
+				desc = "Maximize current buffer Vertically with animation",
+				mode = "n",
+			},
+			{
+				"<C-w>=",
+				"<CMD>WindowsEqualize<CR>",
+				desc = "Equalize all buffers with animation",
+				mode = "n",
+			},
+			{
+				"<C-w>!",
+				"<CMD>WindowsToggleAutowidth<CR>",
+				desc = "Toggle Sliding buffer width",
+				mode = "n",
+			},
+		},
 
-    config = function()
-      require("windows").setup({
-        autowidth = {
-          winwidth = 8,
-          filetype = {
-            NvimTree = 2,
-          },
-        },
-        ignore = {
-          buftype = { "quickfix" },
-          filetype = {
-            "undotree",
-            "gundo",
-            "notify",
-            "DiffviewFiles",
-            "Trouble",
-          },
-        },
-        animation = {
-          enable = true,
-          duration = 50,
-          fps = 30,
-          easing = "in_out_sine",
-        },
-      })
+		config = function()
+			require("windows").setup({
+				autowidth = {
+					winwidth = 8,
+					filetype = {
+						NvimTree = 2,
+					},
+				},
+				ignore = {
+					buftype = { "quickfix" },
+					filetype = {
+						"undotree",
+						"gundo",
+						"notify",
+						"DiffviewFiles",
+						"Trouble",
+					},
+				},
+				animation = {
+					enable = true,
+					duration = 50,
+					fps = 30,
+					easing = "in_out_sine",
+				},
+			})
 
-      vim.cmd("WindowsDisableAutowidth")
-    end,
-  },
-  {
-    "mrjones2014/smart-splits.nvim",
+			vim.cmd("WindowsDisableAutowidth")
+		end,
+	},
+	{
+		"mrjones2014/smart-splits.nvim",
 
-    keys = {
-      "<M-h>",
-      "<M-j>",
-      "<M-k>",
-      "<M-l>",
-      "<C-h>",
-      "<C-j>",
-      "<C-k>",
-      "<C-l>",
-    },
+		keys = {
+			"<M-h>",
+			"<M-j>",
+			"<M-k>",
+			"<M-l>",
+			"<C-h>",
+			"<C-j>",
+			"<C-k>",
+			"<C-l>",
+		},
 
-    config = function()
-      local smart_split = require("smart-splits")
+		config = function()
+			local smart_split = require("smart-splits")
 
-      smart_split.setup({
-        ignored_filetypes = {
-          "nofile",
-          "quickfix",
-          "prompt",
-          "TelescopePrompt",
-        },
-        multiplexer_integration = true,
-      })
+			smart_split.setup({
+				ignored_filetypes = {
+					"nofile",
+					"quickfix",
+					"prompt",
+					"TelescopePrompt",
+				},
+				multiplexer_integration = true,
+			})
 
-      vim.keymap.set("n", "<M-k>", require('smart-splits').resize_up, { desc = "Resize window up" })
-      vim.keymap.set("n", "<M-j>", require('smart-splits').resize_down, { desc = "Resize window down" })
-      vim.keymap.set("n", "<M-h>", require('smart-splits').resize_left, { desc = "Resize window left" })
-      vim.keymap.set("n", "<M-l>", require('smart-splits').resize_right, { desc = "Resize window right" })
-      vim.keymap.set("n", "<C-k>", require('smart-splits').move_cursor_up, { desc = "Move to window up" })
-      vim.keymap.set("n", "<C-j>", require('smart-splits').move_cursor_down, { desc = "Move to window down" })
-      vim.keymap.set("n", "<C-h>", require('smart-splits').move_cursor_left, { desc = "Move to window left" })
-      vim.keymap.set("n", "<C-l>", require('smart-splits').move_cursor_right, { desc = "Move to window right" })
-    end,
-  },
-  {
-    "ahmedkhalf/project.nvim",
-    event = "BufEnter",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-    },
-    keys = {
-      { "<leader>fp", "<CMD>Telescope projects<CR>", desc = "Search recent porjects" },
-    },
-    config = function()
-      require("project_nvim").setup({
-        silent_chdir = true,
-      })
-      require("telescope").load_extension("projects")
-    end
-  },
-  {
-    "SmiteshP/nvim-navic",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-    },
-    config = function()
-      local navic = require("nvim-navic")
-      local icons = require("settings.icons")
-      navic.setup({
-        icons = icons.lsp,
-        lsp = {
-          auto_attach = true,
-          preference = {
-            "volar",
-          },
-        },
-        highlight = false,
-        separator = " ❯ ",
-        depth_limit = 3,
-        depth_limit_indicator = "…",
-        safe_output = true,
-      })
-    end,
-  },
+			vim.keymap.set("n", "<M-k>", require("smart-splits").resize_up, { desc = "Resize window up" })
+			vim.keymap.set("n", "<M-j>", require("smart-splits").resize_down, { desc = "Resize window down" })
+			vim.keymap.set("n", "<M-h>", require("smart-splits").resize_left, { desc = "Resize window left" })
+			vim.keymap.set("n", "<M-l>", require("smart-splits").resize_right, { desc = "Resize window right" })
+			vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up, { desc = "Move to window up" })
+			vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down, { desc = "Move to window down" })
+			vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left, { desc = "Move to window left" })
+			vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right, { desc = "Move to window right" })
+		end,
+	},
+	{
+		"ahmedkhalf/project.nvim",
+		event = "BufEnter",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+		},
+		keys = {
+			{ "<leader>fp", "<CMD>Telescope projects<CR>", desc = "Search recent porjects" },
+		},
+		config = function()
+			require("project_nvim").setup({
+				silent_chdir = true,
+			})
+			require("telescope").load_extension("projects")
+		end,
+	},
+	{
+		"SmiteshP/nvim-navic",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			local navic = require("nvim-navic")
+			local icons = require("settings.icons")
+			navic.setup({
+				icons = icons.lsp,
+				lsp = {
+					auto_attach = true,
+					preference = {
+						"volar",
+					},
+				},
+				highlight = false,
+				separator = " ❯ ",
+				depth_limit = 3,
+				depth_limit_indicator = "…",
+				safe_output = true,
+			})
+		end,
+	},
 }
--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,172 +1,193 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "folke/trouble.nvim",
-      "nvim-lua/plenary.nvim",
-      {
-        "nvim-telescope/telescope-dap.nvim",
-        config = function()
-          require("telescope").load_extension("dap")
-        end,
-      },
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
-        config = function()
-          require("telescope").load_extension("fzf")
-        end,
-      },
-    },
-    keys = {
-      { "<leader>ff", "<CMD>Telescope find_files<CR>", mode = "n", desc = "Find files" },
-      { "<leader>fk", "<CMD>Telescope keymaps<CR>",    mode = "n", desc = "Find keymaps" },
-      { "<leader>fb", "<CMD>Telescope buffers<CR>",    mode = "n", desc = "Find buffers" },
-      { "<leader>fg", "<CMD>Telescope live_grep<CR>",  mode = "n", desc = "Find string in workspace" },
-      { "<leader>fh", "<CMD>Telescope help_tags<CR>",  mode = "n", desc = "Search help" },
-      {
-        "<leader>fl",
-        "<CMD>Telescope current_buffer_fuzzy_find<CR>",
-        mode = "n",
-        desc = "Find line in current buffer"
-      },
-    },
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"folke/trouble.nvim",
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-dap.nvim",
+				config = function()
+					require("telescope").load_extension("dap")
+				end,
+			},
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+				config = function()
+					require("telescope").load_extension("fzf")
+				end,
+			},
+		},
+		keys = {
+			{ "<leader>ff", "<CMD>Telescope find_files<CR>", mode = "n", desc = "Find files" },
+			{ "<leader>fk", "<CMD>Telescope keymaps<CR>", mode = "n", desc = "Find keymaps" },
+			{ "<leader>fb", "<CMD>Telescope buffers<CR>", mode = "n", desc = "Find buffers" },
+			{ "<leader>fg", "<CMD>Telescope live_grep<CR>", mode = "n", desc = "Find string in workspace" },
+			{
+				"<leader>fG",
+				function()
+					require("telescope.builtin").live_grep({
+						vimgrep_arguments = {
+							"rg",
+							"--color=never",
+							"--no-heading",
+							"--with-filename",
+							"--line-number",
+							"--column",
+							"--smart-case",
+							"-uu" --no-ignore --hidden
+						},
+					})
+				end,
+				mode = "n",
+				desc = "Find string in workspace, including hidden files",
+			},
+			{ "<leader>fh", "<CMD>Telescope help_tags<CR>", mode = "n", desc = "Search help" },
+			{
+				"<leader>fl",
+				"<CMD>Telescope current_buffer_fuzzy_find<CR>",
+				mode = "n",
+				desc = "Find line in current buffer",
+			},
+		},
 
-    config = function(_, opts)
-      local telescope = require("telescope")
-      local actions = require("telescope.actions")
+		config = function(_, opts)
+			local telescope = require("telescope")
+			local actions = require("telescope.actions")
 
-      -- Setup telescope config
-      opts = vim.tbl_deep_extend("force", opts, {
-        defaults = {
-          layout_strategy = "flex",
-          mappings = {
-            n = {
-              ["q"] = actions.close,
-              ["<Esc>"] = actions.close,
-              ["<C-n>"] = actions.move_selection_next,
-              ["<C-p>"] = actions.move_selection_previous,
-              ["<C-t>"] = actions.select_tab,
-              ["<C-f>"] = actions.preview_scrolling_up,
-              ["<C-b>"] = actions.preview_scrolling_down,
-            },
-            i = {
-              ["<Esc>"] = actions.close,
-              ["<C-u>"] = false, -- Clear the prompt
-              ["<C-s>"] = actions.select_horizontal,
-              ["<C-t>"] = actions.select_tab,
-              ["<C-f>"] = actions.preview_scrolling_up,
-              ["<C-b>"] = actions.preview_scrolling_down,
-            },
-          },
-          file_ignore_patterns = {
-            "node_modules",
-          },
-        },
-        pickers = {
-          lsp_code_actions = {
-            theme = "cursor",
-          },
-        },
-      })
+			-- Setup telescope config
+			opts = vim.tbl_deep_extend("force", opts, {
+				defaults = {
+					layout_strategy = "flex",
+					mappings = {
+						n = {
+							["q"] = actions.close,
+							["<Esc>"] = actions.close,
+							["<C-n>"] = actions.move_selection_next,
+							["<C-p>"] = actions.move_selection_previous,
+							["<C-t>"] = actions.select_tab,
+							["<C-f>"] = actions.preview_scrolling_up,
+							["<C-b>"] = actions.preview_scrolling_down,
+						},
+						i = {
+							["<Esc>"] = actions.close,
+							["<C-u>"] = false, -- Clear the prompt
+							["<C-s>"] = actions.select_horizontal,
+							["<C-t>"] = actions.select_tab,
+							["<C-f>"] = actions.preview_scrolling_up,
+							["<C-b>"] = actions.preview_scrolling_down,
+						},
+					},
+					file_ignore_patterns = {
+						"node_modules",
+					},
+				},
+				pickers = {
+					lsp_code_actions = {
+						theme = "cursor",
+					},
+				},
+			})
 
-      telescope.setup(opts)
+			telescope.setup(opts)
 
-      -- Filetype update
-      vim.list_extend(settings.ft.exclude_winbar.filetype, {
-        "TelescopePrompt",
-        "TelescopeResults",
-      })
-    end,
-  },
-  {
-    "axieax/urlview.nvim",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-    },
-    keys = {
-      { "<leader>fu",  "<Cmd>UrlView<CR>",      mode = { "n", "v" }, desc = "Show all buffer links" },
-      { "<leader>fub", "<Cmd>UrlView<CR>",      mode = { "n", "v" }, desc = "Show all buffer links" },
-      { "<leader>fup", "<Cmd>UrlView lazy<CR>", mode = { "n", "v" }, desc = "Show all plugin links" },
-      { "[u" },
-      { "]" }
-    },
-    config = function()
-      require("urlview").setup({
-        default_picker = "telescope",
-        default_prefix = "https://",
-        default_action = "system",
-        unique = true,
-        sorted = true,
-        jump = {
-          ["prev"] = "[u",
-          ["next"] = "]u",
-        },
-      })
-    end,
-  },
-  {
-    "FeiyouG/commander.nvim",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-    },
-    commander = {
-      {
-        desc = "Open Commander",
-        cmd = function() require("commander").show() end,
-        keys = {
-          { "n", "<leader>f" },
-          { "n", "<leader>fc" },
-        },
-      }
-    },
-    opts = {
-      components = {
-        "DESC",
-        "KEYS",
-        "CAT",
-      },
-      sort_by = {
-        "DESC",
-        "KEYS",
-        "CAT",
-        "CMD"
-      },
+			-- Filetype update
+			vim.list_extend(settings.ft.exclude_winbar.filetype, {
+				"TelescopePrompt",
+				"TelescopeResults",
+			})
+		end,
+	},
+	{
+		"axieax/urlview.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+		},
+		keys = {
+			{ "<leader>fu", "<Cmd>UrlView<CR>", mode = { "n", "v" }, desc = "Show all buffer links" },
+			{ "<leader>fub", "<Cmd>UrlView<CR>", mode = { "n", "v" }, desc = "Show all buffer links" },
+			{ "<leader>fup", "<Cmd>UrlView lazy<CR>", mode = { "n", "v" }, desc = "Show all plugin links" },
+			{ "[u" },
+			{ "]" },
+		},
+		config = function()
+			require("urlview").setup({
+				default_picker = "telescope",
+				default_prefix = "https://",
+				default_action = "system",
+				unique = true,
+				sorted = true,
+				jump = {
+					["prev"] = "[u",
+					["next"] = "]u",
+				},
+			})
+		end,
+	},
+	{
+		"FeiyouG/commander.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+		},
+		commander = {
+			{
+				desc = "Open Commander",
+				cmd = function()
+					require("commander").show()
+				end,
+				keys = {
+					{ "n", "<leader>f" },
+					{ "n", "<leader>fc" },
+				},
+			},
+		},
+		opts = {
+			components = {
+				"DESC",
+				"KEYS",
+				"CAT",
+			},
+			sort_by = {
+				"DESC",
+				"KEYS",
+				"CAT",
+				"CMD",
+			},
 
-      integration = {
-        telescope = {
-          enable = true,
-        },
-        lazy = {
-          enable = true,
-          set_plugin_name_as_cat = true
-        }
-      }
-    }
-  },
-  {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = {
-      {
-        "nvim-telescope/telescope.nvim",
-        opts = function(_, opts)
-          return vim.tbl_deep_extend("force", opts, {
-            extensions = {
-              file_browser = {
-                theme = "dropdown",
-                hijack_netrw = false,
-              }
-            }
-          })
-        end,
-      },
-      "nvim-lua/plenary.nvim",
-    },
-    keys = {
-      { "<leader>ft", "<CMD>Telescope file_browser<CR>", desc = "Browse files with telescope" },
-    },
-    config = function()
-      require("telescope").load_extension("file_browser")
-    end
-  }
+			integration = {
+				telescope = {
+					enable = true,
+				},
+				lazy = {
+					enable = true,
+					set_plugin_name_as_cat = true,
+				},
+			},
+		},
+	},
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = {
+			{
+				"nvim-telescope/telescope.nvim",
+				opts = function(_, opts)
+					return vim.tbl_deep_extend("force", opts, {
+						extensions = {
+							file_browser = {
+								theme = "dropdown",
+								hijack_netrw = false,
+							},
+						},
+					})
+				end,
+			},
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>ft", "<CMD>Telescope file_browser<CR>", desc = "Browse files with telescope" },
+		},
+		config = function()
+			require("telescope").load_extension("file_browser")
+		end,
+	},
 }
